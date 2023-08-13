fd77b51a4e442ce610102cc8ec1c525cec971738
Nghia <75264221+dangnghia2101@users.noreply.github.com>
2023-08-13 08:53:48 +0700

    fix: search screen

---
+++ shared/src/commonMain/kotlin/screens/components/search/SearchItem.kt
@@ -0,0 +1,44 @@
+package screens.components.search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import theme.medium14
+import utils.AsyncImage
+
+@Composable
+fun SearchItem(){
+    Column(modifier = Modifier.fillMaxWidth().padding(top = 39.dp, start = 9.dp, end = 9.dp)) {
+        AsyncImage(
+            imageUrl = "https://m.media-amazon.com/images/I/918Ai9a893L._AC_UF1000,1000_QL80_.jpg",
+            modifier = Modifier.height(250.dp).fillMaxWidth().clip(RoundedCornerShape(20.dp)),
+            filterQuality = DrawScope.DefaultFilterQuality,
+            contentDescription = "Now playing movie",
+            contentScale = ContentScale.Crop,
+            loadingPlaceHolder = {},
+            errorPlaceHolder = {},
+            alignment = Alignment.Center,
+            alpha = DefaultAlpha,
+            colorFilter = null
+        )
+
+        Text(
+            "Shazam",
+            style = MaterialTheme.typography.medium14,
+            modifier = Modifier.padding(top = 10.dp).align(Alignment.CenterHorizontally)
+        )
+    }
+}
--- shared/src/commonMain/kotlin/screens/search/search/SearchScreen.kt
+++ shared/src/commonMain/kotlin/screens/search/search/SearchScreen.kt
@@ -1,20 +1,86 @@
 package screens.search.search
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import moe.tlaster.precompose.navigation.Navigator
+import screens.components.search.SearchItem
+import theme.Primary
+import theme.Tranparent
+import theme.regular14LightGray
 
 
 @Composable
 fun SearchScreen(
     navigator: Navigator
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(
-            "SearchScreen"
-        )
-    }
+    var search by remember { mutableStateOf("") }
+
+//    Column (modifier = Modifier.fillMaxSize().padding(top = 35.dp)) {
+        Column (modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.padding(horizontal = 30.dp).padding(top = 39.dp)) {
+                    TextField(
+                        modifier = Modifier.fillMaxWidth().border(2.dp, MaterialTheme.colors.secondary, shape = RoundedCornerShape(20.dp)),
+                        value = search,
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = MaterialTheme.colors.background,
+                            cursorColor = MaterialTheme.colors.secondary,
+                            focusedIndicatorColor = Tranparent,
+                            unfocusedIndicatorColor = Tranparent,
+                        ),
+                        onValueChange = {
+                            search = it
+                        },
+                        singleLine = true,
+                        trailingIcon = {
+                            Icon(
+                                Icons.Filled.Search,
+                                contentDescription = "search",
+                                tint = Primary
+                            )
+                        },
+                        placeholder = {
+                            Text(
+                                "Search your Movies...",
+                                style = MaterialTheme.typography.regular14LightGray,
+                            )
+                        }
+                    )
+                }
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(10.dp),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(5) {
+                    SearchItem()
+                }
+            }
+        }
+
 }
